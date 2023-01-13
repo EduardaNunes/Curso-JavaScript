bd1433911c84f17a5b383d83579fcc85b0012f8c
Duda Nunes <eduardapmn@gmail.com>
2023-01-12 21:30:33 -0300

    commit exercício 1

---
+++ Exercicio_01/ex01.js
@@ -0,0 +1,37 @@
+var clique = 1;
+        // Observação IMPORTANTE -> mouseenter é tanto para o pai quanto para os filhos, enquanto mouseover é apenas para o pai, 
+        // logo, se passar o mouse por cima dos filhos conta como se tivesse saido.
+        function entrou(){
+            window.document.getElementById('bloco1').innerText='Oi, mouse! :)';
+        }
+
+        // Observação IMPORTANTE -> mouseleave é tanto para o pai quanto para os filhos, enquanto mouseover é apenas para o pai, 
+        // logo, se passar o mouse por cima dos filhos conta como se tivesse saido.
+        function saiu(){
+            window.document.getElementById('bloco1').innerText='Tchau, mouse :(';
+        }
+
+        function clicou(){
+            window.document.getElementById('bloco2').innerText=`Clicou ${clique} vezes`;
+            clique++;
+        }
+
+        function segurou(){
+            window.document.getElementById('bloco3').innerText='Hihi faz cócegas';
+        }
+
+        function soltou(){
+            window.document.getElementById('bloco3').innerText='Agora, tente segurar o clique';
+        }
+
+        function moveu(){
+            window.document.getElementById('bloco4').innerText='CALMAAA!';
+        }
+
+        function saiu2(){
+            window.document.getElementById('bloco4').innerText='Mova o mouse aqui dentro!';
+        }
+
+        function reset(){
+            location.reload()
+        }
