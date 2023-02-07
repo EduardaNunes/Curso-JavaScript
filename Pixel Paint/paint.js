59fb90b4cbee9d46d719431bc8b93eba111da507
Duda Nunes <eduardapmn@gmail.com>
2023-02-07 14:53:59 -0300

    3 new canvas size options

--- Pixel Paint/paint.js
+++ Pixel Paint/paint.js
@@ -1,19 +1,69 @@
+var canvas = document.querySelector('.pixel-canvas')
+
 var pixel = document.querySelectorAll('.pixel-paint')
 var colors = document.querySelectorAll('.color')
 var color = ''
 
-for(var i = 0; i<pixel.length; i++){
-    pixel[i].addEventListener('click', pixelPaint)
+paint();
+
+function size3(){
+    canvas.innerHTML=''
+    for (var i=0; i<9; i++){
+        canvas.innerHTML+='<div class="pixel-paint"></div>'
+    }
+    canvas.style.width='32vh'
+    canvas.style.height='32vh'
+    canvas.style.gridTemplateColumns='repeat(3,1fr)'
+    canvas.style.gridTemplateRows='repeat(3,1fr)'
+    
+    pixel = document.querySelectorAll('.pixel-paint')
+    paint()
 }
 
- for(var c = 0; c<colors.length; c++){
-    colors[c].addEventListener('click', paintBrush)
+function size4(){
+    canvas.innerHTML=''
+    for (var i=0; i<16; i++){
+        canvas.innerHTML+='<div class="pixel-paint"></div>'
+    }
+    canvas.style.width='43vh'
+    canvas.style.height='43vh'
+    canvas.style.gridTemplateColumns='repeat(4,1fr)'
+    canvas.style.gridTemplateRows='repeat(4,1fr)'
+
+    pixel = document.querySelectorAll('.pixel-paint')
+    paint()
 }
 
-function paintBrush(){
-     color = window.getComputedStyle(this).backgroundColor;
+function size5(){
+    canvas.innerHTML=''
+    for (var i=0; i<25; i++){
+        canvas.innerHTML+='<div class="pixel-paint"></div>'
+    }
+    canvas.style.width='54vh'
+    canvas.style.height='54vh'
+    canvas.style.gridTemplateColumns='repeat(5,1fr)'
+    canvas.style.gridTemplateRows='repeat(5,1fr)'
+
+    pixel = document.querySelectorAll('.pixel-paint')
+    paint()
 }
 
-function pixelPaint(){
-    this.style.background = color;
+function paint(){
+
+    for(var i = 0; i<pixel.length; i++){
+        pixel[i].addEventListener('click', pixelPaint)
+    }
+
+    for(var c = 0; c<colors.length; c++){
+        colors[c].addEventListener('click', paintBrush)
+    }
+
+    function paintBrush(){
+        color = window.getComputedStyle(this).backgroundColor;
+    }
+
+    function pixelPaint(){
+        console.log(pixel)
+        this.style.background = color;
+    }
 }
